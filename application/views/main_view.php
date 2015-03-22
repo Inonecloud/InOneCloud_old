bc7d55845ad698a85efda248c3da9b39016c24ef
Andrew <andrewyelmanov@gmail.com>
2015-03-22 17:50:22 +0300

    ver 0.0.1.2
    Переделана главная страница, еще не подключена база данных

---
+++ application/views/main_view.php
@@ -0,0 +1,12 @@
+<div id="cover">
+	<h2>Welcome to InOneCloud</h2>
+	<form id="auth" method="post" action="index.php">
+		<fieldset id="inputs">
+			<input id="username" name="username" type="text" placeholder="Username" autofocus required>
+			<input id="password" name="password" type="password" placeholder="Password" required>
+		</fieldset>
+		<fieldset id="actions">
+			<input id="submit" type ="submit" name="logIN" value = "Log in">
+		</fieldset>
+	</form>
+</div>
---
+++ application/views/template_view.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/css/styles.css">
+	<link rel="icon" type="image/ico" href="favicon.ico">
+	<title>InOneCloud</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<h1 id="logo">InOneCloud</h1>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Registration</a></li>				
+			</ul>
+		</nav>
+		<?php include 'application/views/'.$content_view; ?>
+		
+	</header>
+	<article>
+		
+	</article>
+	<footer>
+		<div id="foot">
+			<p>Andrew Yelmanov (c) 2015</p>
+			<div id="footermenu">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Registration</a></li>				
+				</ul>
+			</div>
+			<div id="partners">
+				<ul>
+					<li><a href="https://drive.google.com">Google Drive</a></li>
+					<li><a href="https.disk.yandex.ru">Yandex Disk</a></li>				
+				</ul>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>

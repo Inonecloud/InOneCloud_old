c2c648ab93e4e72d022b2e359dea07517229a1e7
Andrew <andrewyelmanov@gmail.com>
2015-03-28 11:41:34 +0300

    ver 0.0.1.3.3
    Добавлен favicon

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -4,7 +4,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="/css/styles.css">
-	<link rel="icon" type="image/ico" href="favicon.ico">
+	<link rel="icon" type="image/ico" href="/img/favicon.ico">
 	<title>InOneCloud</title>
 </head>
 <body>

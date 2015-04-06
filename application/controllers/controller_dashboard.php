182598b43f26719ddbb8f88ef7fbc92adf5360a9
Andrew <andrewyelmanov@gmail.com>
2015-04-06 23:10:17 +0300

    ver 0.0.1.8.1

--- application/controllers/controller_dashboard.php
+++ application/controllers/controller_dashboard.php
@@ -25,17 +25,18 @@ class Controller_Dashboard extends Controller
 	{
 		$client_id = "d0387d6c503246909145797d469d7248";
 		$client_secret = "576b5cf52f1b4f1bab1eb7eeca1db60f";
-		echo YDconnect::init($client_id,$client_secret); 
-		echo "hello";
+		$yatoken = YDconnect::init($client_id,$client_secret); 
 		//echo YDconnect::get_code();
 		//echo $tocken;
-
+		Session::set('yatoken', $yatoken);
+		header('location: ..');
+		return $yatoken;
 	}
 
 	function action_logout()
 	{
 		Session::destroy();
-		header('location: ../login');
+		header('location: ../');
 		exit;
 	}
 }
--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -1,6 +1,8 @@
 <?php if(Session::get('loggedIn') == true):?>			
 Hello 
 <?php $username = Session::get('username');?>
+<?php $yatocken = Session::get('yatoken');?>
 <?php endif; ?>		
-<?php echo $username; ?>
+<?php echo $username . "<br/>"; ?>
+<?php echo $yatocken; ?>
 <a href="dashboard/yandex_connect">Подключиться к Яндексу</a>

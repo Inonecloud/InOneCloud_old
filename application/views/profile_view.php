4fe0fa9deffea1976a1c1d92b8cb66720b7db4e6
Andrew <andrewyelmanov@gmail.com>
2015-04-17 17:13:15 +0300

    ver 0.0.2.1

---
+++ application/views/profile_view.php
@@ -0,0 +1,6 @@
+<div id='profile'>
+		<form method='post' action='upload2.php' enctype='multipart/form-data'>
+			<label>Загрузить файл</label> <input type='file' name='filename' size='10'/>
+			<input type='submit' value='Загрузить'/>
+		</form>
+</div>

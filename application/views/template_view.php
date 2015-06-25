79144be38882bb098370a92f05c8345d8bed0168
Andrew <andrewyelmanov@gmail.com>
2015-06-25 22:48:36 +0300

    ver 0.0.3.4

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -3,16 +3,27 @@
 <head>
 	<meta charset = "utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="js/init.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
-	<link rel="stylesheet" type="text/css" href="css/styles-normal.css">
 	<link rel="icon" type="image/ico" href="/img/favicon.ico">
 	<title>InOneCloud</title>
+	<script type="text/javascript">
+		function ChangeText()
+		{
+			//document.body.style.backgroundColor="lavender";
+			document.getElementById("caption").innerHTML="¯\\_(ツ)_/¯ What's next?";
+			document.getElementById("caption").style.fontFamily="Segoe UI";
+			document.getElementById("caption").style.fontSize="2.2em";
+		}
+	</script>
 </head>
 <body>
+
+
+
+
 	<!--Top Menu-->
-	<header id="logo">
-		<h1>InOneCloud</h1>
+	<header id="logo" onclick="ChangeText()" >
+		<h1 id = "caption">InOneCloud</h1>
 		<nav>
 			<ul class="topmenu">
 				<?php if(Session::get('loggedIn') == true):?>
@@ -39,9 +50,15 @@
 		</ul>
 		<nav class="buttonmenu">
 			<ul class="repeatemenu">
-				<li><a href="">Home</a></li>
-				<li><a href="">About</a></li>
-				<li><a href="">Registration</a></li>
+				<?php if(Session::get('loggedIn') == true):?>
+				<li><a href="dashboard">Home</a></li>
+				<li><a href="#">Settings</a></li>
+				<li><a href="dashboard/logout">Log out</a></li>
+				<?php else: ?>
+				<li><a href="/">Home</a></li>
+				<li><a href="about">About</a></li>
+				<li><a href="registration">Registration</a></li>
+				<?php endif; ?>	
 			</ul>
 			<ul class="storages">
 				<li><a href="https://drive.google.com">Google Drive</a></li>

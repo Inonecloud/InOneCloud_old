87d3869a6b0aecf228c52a05f62f931b050ef4d6
Andrew <andrewyelmanov@gmail.com>
2015-11-06 14:58:22 +0300

    ver 0.0.3.6.7.2
    Interface updates

--- application/views/registration_view.php
+++ application/views/registration_view.php
@@ -14,7 +14,7 @@
 
 				<p>You can create your account here</p>
 			</header>
-</article>-->
+		</article>-->
 
 <article id="main" class="container">	
 	<header class="aboutcloud">
@@ -27,12 +27,19 @@
 			<fieldset id="inputs">
 				<input class ="username" name="username" type="text" placeholder="Username" autofocus required>
 				<input class="email" name="email" type="email" placeholder="Email" required>
-				<input class="password" name="password" type="password" placeholder="Password" required>
+				<input class="password" name="password" type="password" placeholder="Password" required pattern="[0-9a-zA-Z]{6,32}" title="Password should have 6 sybols with digits">
 				<input class="password" name="CNFpassword" type="password" placeholder="Repeat password" required>
+				<select name="lang">
+					<option></option>
+					<option value="en">English</option>
+					<option value="de">Deutsch</option>
+					<option value="ru">Русский</option>
+				</select>
 			</fieldset>
 			<fieldset id="actions">
 				<input type="checkbox" required><label>I agree with license</label>
-				<input class="submit" type ="submit" name="Registration" value = "Create account">				</fieldset>
+				<input class="submit" type ="submit" name="Registration" value = "Create account">
+			</fieldset>
 		</form>	
 	</article>	
 </article> 
@@ -67,5 +74,5 @@
 				return true;
 				//alert("Пароли должны совпадать");
 		}
-		</script>-->
+	</script>-->
 </div>
